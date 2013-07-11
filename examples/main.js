67fd03592ba4d36634ac58614659aca0d71244b8
Edgar Hipp <hipp.edg@gmail.com>
2013-07-11 16:45:13 +0200

    added demo.html

---
+++ examples/main.js
@@ -0,0 +1,71 @@
+DocUtils.loadDoc("tagExample.docx")
+DocUtils.loadDoc("tagLooping.docx")
+
+function textAreaAdjust(o) {
+    o.style.height = "1px";
+    o.style.height = (25+o.scrollHeight)+"px";
+}
+
+window.onload=  function () {
+
+var textAreaList= document.getElementsByTagName('textarea');
+
+for (var i = textAreaList.length - 1; i >= 0; i--) {
+	textAreaAdjust(textAreaList[i])
+	var executeButton=document.createElement('button')
+	executeButton.className = "execute";
+	executeButton.innerHTML="Execute";
+	textAreaList[i].parentNode.insertBefore(executeButton, textAreaList[i].nextSibling);
+
+	var viewRawButton=document.createElement('button')
+	viewRawButton.className = "raw";
+	viewRawButton.innerHTML="View raw Document";
+	textAreaList[i].parentNode.insertBefore(viewRawButton, textAreaList[i].nextSibling);
+};
+
+var executeButtonList= document.getElementsByClassName('execute');
+
+for (var i = 0; i < executeButtonList.length; i++) {
+	executeButtonList[i].onclick=function()
+	{
+		childs=(this.parentNode.childNodes)
+
+		for (var j = 0; j < childs.length; j++) {
+			if(childs[j].tagName=='TEXTAREA')
+			{
+				console.log(childs[j].value)	
+				eval(childs[j].value)
+			}
+		};
+	}
+};
+
+
+var viewRawButtonList= document.getElementsByClassName('raw');
+
+
+for (var i = 0; i < viewRawButtonList.length; i++) {
+	viewRawButtonList[i].onclick=function()
+	{
+		childs=(this.parentNode.childNodes)
+
+		for (var j = 0; j < childs.length; j++) {
+			if(childs[j].tagName=='TEXTAREA')
+			{
+				raw=(childs[j].getAttribute("raw"))
+				console.log	(raw)
+				var doc=new DocxGen(docXData[raw])
+				doc.output()
+			}
+		};
+
+	}
+
+
+	
+};
+
+
+
+}
+
