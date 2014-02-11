609a5413e6ca9e0c9fef9cd5877071b362c34e04
Edgar HIPP <hipp.edg@gmail.com>
2014-02-11 11:47:21 +0100

    added automatic jasmine-node after saving a coffee file

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var plumber= require('gulp-plumber');
 var coffee= require('gulp-coffee');
 var concat= require('gulp-concat');
 var uglify= require('gulp-uglify');
+var spawn = require('child_process').spawn;
 
 var paths = {
   coffee: ['coffee/templaterState.coffee','coffee/docxgen.coffee','coffee/docUtils.coffee','coffee/imgReplacer.coffee','coffee/docxQrCode.coffee','coffee/xmlTemplater.coffee','coffee/docxTemplater.coffee'], // compile individually into dest, maintaining folder structure
@@ -12,17 +13,67 @@ var paths = {
 };
 
 gulp.task('watch', function () {
-	gulp.watch(paths.coffee,['coffee']);
+	gulp.watch(paths.coffee,['coffee','jasmine']);
 	gulp.watch(paths.coffeeTest,['coffeeTest']);
 });
 
-gulp.task('coffee', function() {
+gulp.task('coffee', function(cb) {
   // Minify and copy all JavaScript (except vendor scripts)
   return gulp.src(paths.coffee)
     .pipe(coffee())
     .pipe(uglify())
     .pipe(concat('docxgen.js'))
     .pipe(gulp.dest('./js/'));
+	cb(err)
+});
+
+gulp.task('jasmine', ['coffee'], function() {
+ testDirectory=__dirname+'\\test\\spec';
+	var child = spawn("cmd", ["/c","jasmine-node","docxgenTest.spec.js"], {cwd:testDirectory});
+            stdout = '',
+            stderr = '';
+
+        child.stdout.setEncoding('utf8');
+
+        child.stdout.on('data', function (data) {
+        	regex=/([0-9]+) tests, ([0-9]+) assertions, ([0-9]+) failures, ([0-9]+) skipped/;
+            if(regex.test(data))
+        	{
+        		matches=regex.exec(data);
+        		tests=parseInt(matches[1]);
+        		assertions=parseInt(matches[2]);
+        		failures=parseInt(matches[3]);
+        		skipped=parseInt(matches[4]);
+
+				if (failures==0)
+					{
+            		gutil.log(gutil.colors.green(data));
+            		}
+            	else
+        			{
+            		gutil.log(gutil.colors.red(data));
+            		gutil.beep()
+            		}
+        	}
+        });
+
+        child.on('error',function(err){
+        	console.log('error executing jasmine',err);
+        });
+
+        child.stderr.setEncoding('utf8');
+        child.stderr.on('data', function (data) {
+            stderr += data;
+            gutil.log(gutil.colors.red(data));
+        });
+
+        child.on('close', function(code) {
+            gutil.log("Done with exit code", code);
+            gutil.log("You access complete stdout and stderr from here"); // stdout, stderr
+        });
+
+
+
 });
 
 gulp.task('coffeeTest', function() {
@@ -36,4 +87,4 @@ gulp.task('coffeeTest', function() {
 
 
 
-gulp.task('default',['coffeeTest','coffee','watch']);
+gulp.task('default',['coffeeTest','coffee','jasmine','watch']);
