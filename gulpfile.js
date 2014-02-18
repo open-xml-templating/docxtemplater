be117bdd997405804b7973921d8727c92d1a52d7
Edgar HIPP <hipp.edg@gmail.com>
2014-02-18 15:58:07 +0100

    removed unnecessary dev dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var watch= require('gulp-watch');
-var plumber= require('gulp-plumber');
 var coffee= require('gulp-coffee');
 var concat= require('gulp-concat');
 var uglify= require('gulp-uglify');
