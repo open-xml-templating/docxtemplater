01678c4e53923bcf72135eee2914cc0a719a5496
Edgar HIPP <hipp.edg@gmail.com>
2015-08-16 19:05:49 +0200

    Put svg inside html directly

--- examples/compile_site.js
+++ examples/compile_site.js
@@ -2,9 +2,14 @@ var Mustache = require('mustache');
 var fs = require('fs');
 
 var view = {
-  title: "Joe",
-  calc: function () {
-    return 2 + 4;
+  features: {
+    easy:fs.readFileSync(__dirname+"/basic14.svg").toString(),
+    everywhere:fs.readFileSync(__dirname+"/cellphone106.svg").toString(),
+    fulldocumentation:fs.readFileSync(__dirname+"/text150.svg").toString(),
+    manytags:fs.readFileSync(__dirname+"/open112.svg").toString(),
+    openextension:fs.readFileSync(__dirname+"/puzzle37.svg").toString(),
+    freebier:fs.readFileSync(__dirname+"/drink24.svg").toString(),
+    fullytested:fs.readFileSync(__dirname+"/test-tube12.svg").toString(),
   }
 };
 
