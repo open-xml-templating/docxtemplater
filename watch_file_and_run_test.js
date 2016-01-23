c7645d0263f5e4902e71acbeec1fa09a04931d66
Edgar HIPP <hipp.edg@gmail.com>
2016-01-23 11:59:48 +0100

    Update scripts

---
+++ watch_file_and_run_test.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var fs = require("fs");
+var path = require("path");
+var exec = require("child_process").exec;
+
+var lastRun = 0;
+var throttleTime = 1000;
+
+var execTests = function () {
+	/* eslint-disable no-console */
+	exec("npm run cp:test && npm run mocha -- --colors", function (error, stdout, stderr) {
+		if (stdout) {
+			console.log("stdout: " + stdout);
+		}
+		if (stderr) {
+			console.log("stderr: " + stderr);
+		}
+		if (error !== null) {
+			console.log("exec error: " + error);
+		}
+	});
+};
+
+fs.watch(path.join(__dirname, "js"), function () {
+	var now = new Date().getTime();
+	if (now < lastRun + throttleTime) {
+		return;
+	}
+	lastRun = now;
+
+	setTimeout(execTests, 10);
+});
+
