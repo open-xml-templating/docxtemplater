e78b4f6d22cfe16392c9223383b7947a698d328e
Porter <porter@cando.com>
2016-08-30 14:46:50 +0000

    remove comment

--- es6/tests/docxtemplater.js
+++ es6/tests/docxtemplater.js
@@ -159,7 +159,6 @@ function startTest() {
 				expect(fullText).to.be.equal("");
 			});
 			it("should load the right template files for the document", function () {
-				// default value document.xml
 				var templatedFiles = (docX["tagExample.docx"].getTemplatedFiles());
 				expect(templatedFiles).to.be.eql(["word/header1.xml", "word/footer1.xml", "word/document.xml"]);
 			});
