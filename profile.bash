dc238c55032fcae9d67872cb07f235ff0141a093
Edgar HIPP <hipp.edg@gmail.com>
2016-11-26 18:16:08 +0100

    Update utility files

--- profile.bash
+++ profile.bash
@@ -3,19 +3,38 @@
 set -e
 set -u
 
-mkdir -p prof/tests
+profile() {
+	find ./js -name '*.js' | while IFS='' read -r filename; do
+		echo "$filename"
+		echo "${filename/js/prof}"
+		mkdir -p "$(dirname "${filename/js/prof}")"
+		profi-stanbul profile --output "${filename/js/prof}" "${filename}"
+	done
+	mocha prof/tests/docxtemplater.js
+}
 
-npm run compile
+analyse() {
+	jqq='. | to_entries | sort_by(.value.ms) | .[].value | .filename + "@" + .name + ":" + (.calls|tostring) + ":" + (.ms|tostring) + "ms"'
 
-find ./js -name '*.js' | while IFS='' read -r filename || [[ -n "$filename" ]]; do
-	echo "$filename"
-	echo "${filename/js/prof}"
-	profi-stanbul profile --output "${filename/js/prof}" "${filename}"
-done
-
-mocha prof/tests/docxtemplater.js
-
-jqq='. | to_entries | sort_by(.value.ms) | .[].value | .filename + "@" + .name + ":" + (.calls|tostring) + ":" + (.ms|tostring) + "ms"'
-
-jq <p.json --raw-output "$jqq" |
+	jq <p.json --raw-output "$jqq" |
 	sed "s:$(pwd)/prof/::g"
+}
+
+compile() {
+	npm run compile
+}
+
+action=${1:-""}
+if [ "$action" = "" ]
+then
+	echo "compile"
+	compile
+	echo "profile"
+	profile >/dev/null
+	echo "analyse"
+	analyse
+else
+	"$action"
+fi
+
+
