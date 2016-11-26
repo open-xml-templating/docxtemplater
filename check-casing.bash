c9a409d612e75655d5d9d846a0f28c02accb3a20
Edgar HIPP <hipp.edg@gmail.com>
2016-11-26 18:30:38 +0100

    Update utility files

--- check-casing.bash
+++ check-casing.bash
@@ -3,8 +3,12 @@
 
 ignored="$(git ls-files -o -i --exclude-standard)"
 all="$(find . -type f | cut -c 3- | grep -v '^.git')"
-trackedfiles="$(( git status --short|cut -d\  -f2- && git ls-files ) | sort -u | ( xargs -d '\n' -- stat -c%n 2>/dev/null  ||: ))"
-count="$(echo "$trackedfiles" | grep -v -E '(Makefile)' | grep -v '^docs/' | grep -v '\.md$' | grep '[A-Z_]' | tee /dev/stderr | wc -l)"
+
+( git status --short|cut -d\  -f2- && git ls-files ) |
+sort -u |
+( xargs -d '\n' -- stat -c%n 2>/dev/null  ||: ) > trackedfiles
+
+count="$(grep -v -E '(Makefile)' <trackedfiles | grep -v '^docs/' | grep -v '\.md$' | grep '[A-Z_]' | tee /dev/stderr | wc -l)"
 if [ "$count" = "0" ]
 then
 	echo "Casing OK"
