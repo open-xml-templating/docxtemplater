07a951c895001f263ac1c19e25c5eb4d7fb937a3
Edgar HIPP <hipp.edg@gmail.com>
2017-09-16 11:56:20 +0200

    Update build browser to use webpack

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+/* eslint-disable no-process-env */
+const min = process.env.MIN === "true";
+
+module.exports = {
+	entry: "./es6/docxtemplater.js",
+	output: {
+		path: path.resolve(__dirname, "build"),
+		filename: `docxtemplater.${min ? "min." : ""}js`,
+		library: "docxtemplater",
+		libraryTarget: "umd",
+	},
+	resolve: {
+		alias: {xmldom: path.resolve(__dirname, "es6/browser-versions/xmldom.js")},
+	},
+	module: {
+		rules: [
+			{
+				test: [/\.js$/],
+				exclude: [/node_modules/],
+				loader: "babel-loader",
+				options: {presets: ["es2015"]},
+			},
+		],
+	},
+	plugins: [].concat(
+		min
+			? [
+				new webpack.optimize.UglifyJsPlugin({
+					compress: {warnings: false},
+				}),
+			] : []
+	),
+};
