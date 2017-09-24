5af8129f7d8d895592640582185b5829bb67d074
Edgar HIPP <hipp.edg@gmail.com>
2017-09-24 16:53:27 +0200

    [tmp] OK

--- webdriver.bash
+++ webdriver.bash
@@ -36,8 +36,4 @@ set +e
 browserName="chrome" platform="Windows 10" version="58" node webdriver.js
 browserName="firefox" platform="Windows 10" version="55" node webdriver.js
 browserName="internet explorer" platform="Windows 10" version="11" node webdriver.js
-browserName="internet explorer" platform="Windows 2008" version="9" node webdriver.js
-browserName="microsoftedge" platform="Windows 10" version="15" node webdriver.js
-browserName="opera" platform="Windows 2008" version="12" node webdriver.js
-browserName="safari" platform="Mac 10.12" version="10.0"  node webdriver.js
 browserName="iphone" platform="Mac 10.11" version="10.2" node webdriver.js
