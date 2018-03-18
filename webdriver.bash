5aed88352f869c1845cd217aeecec754c9b774da
Edgar HIPP <hipp.edg@gmail.com>
2018-03-18 20:22:06 +0100

    Add MicrosoftEdge tests

--- webdriver.bash
+++ webdriver.bash
@@ -28,6 +28,8 @@ then
 fi
 
 result=0
+
+browserName="MicrosoftEdge" platform="Windows 10" version="16.16299" node webdriver.js || result=1
 browserName="safari" platform="macOs 10.12" version="11.0" node webdriver.js || result=1
 browserName="chrome" platform="Windows 10" version="58" node webdriver.js || result=1
 browserName="firefox" platform="Windows 10" version="55" node webdriver.js || result=1
