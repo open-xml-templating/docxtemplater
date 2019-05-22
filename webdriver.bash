55d3383515aa2506ef117ae0de7c39804dc657e2
Edgar HIPP <hipp.edg@gmail.com>
2019-05-22 11:24:28 +0200

    Move webdriver-saucelabs code

--- webdriver.bash
+++ webdriver.bash
@@ -47,24 +47,7 @@ then
 	fi
 	node webdriver.js
 	exit "$?"
+else
+	bash webdriver-saucelabs.bash
 fi
 
-result=0
-
-browserName="MicrosoftEdge" platform="Windows 10" version="16.16299" node webdriver.js || result=1
-browserName="MicrosoftEdge" platform="Windows 10" version="17.17134" node webdriver.js || result=1
-browserName="MicrosoftEdge" platform="Windows 10" version="18.17763" node webdriver.js || result=1
-browserName="safari" platform="macOs 10.12" version="11.0" filter="Speed test" node webdriver.js || result=1
-browserName="safari" platform="macOs 10.13" version="12.0" filter="Speed test" node webdriver.js || result=1
-browserName="chrome" platform="Windows 10" version="58" node webdriver.js || result=1
-browserName="chrome" platform="Windows 10" version="71" node webdriver.js || result=1
-browserName="chrome" platform="Windows 10" version="73" node webdriver.js || result=1
-browserName="firefox" platform="Windows 10" version="55" node webdriver.js || result=1
-browserName="firefox" platform="Windows 10" version="60" node webdriver.js || result=1
-browserName="firefox" platform="Windows 10" version="64" node webdriver.js || result=1
-browserName="firefox" platform="Windows 10" version="65" node webdriver.js || result=1
-browserName="firefox" platform="Windows 10" version="66" node webdriver.js || result=1
-browserName="internet explorer" platform="Windows 7" filter="Speed test" version="10.0" node webdriver.js || result=1
-browserName="internet explorer" platform="Windows 10" filter="Speed test" version="11" node webdriver.js || result=1
-browserName="iphone" platform="Mac 10.11" version="10.3" node webdriver.js || result=1
-exit "$result"
