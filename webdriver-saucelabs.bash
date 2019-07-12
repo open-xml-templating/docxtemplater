e38cf73a89ea873e07d9d5c4c018e1478b3c6418
Edgar HIPP <hipp.edg@gmail.com>
2019-07-12 11:40:38 +0200

    Add webdriver-saucelabs browsers

--- webdriver-saucelabs.bash
+++ webdriver-saucelabs.bash
@@ -10,11 +10,14 @@ browserName="safari" platform="macOs 10.12" version="11.0" filter="Speed test" n
 browserName="chrome" platform="Windows 10" version="58" node webdriver.js || result=1
 browserName="chrome" platform="Windows 10" version="71" node webdriver.js || result=1
 browserName="chrome" platform="Windows 10" version="73" node webdriver.js || result=1
+browserName="chrome" platform="Windows 10" version="75" node webdriver.js || result=1
 browserName="firefox" platform="Windows 10" version="55" node webdriver.js || result=1
 browserName="firefox" platform="Windows 10" version="60" node webdriver.js || result=1
 browserName="firefox" platform="Windows 10" version="64" node webdriver.js || result=1
 browserName="firefox" platform="Windows 10" version="65" node webdriver.js || result=1
 browserName="firefox" platform="Windows 10" version="66" node webdriver.js || result=1
+browserName="firefox" platform="Windows 10" version="67" node webdriver.js || result=1
+browserName="firefox" platform="Windows 10" version="68" node webdriver.js || result=1
 browserName="internet explorer" platform="Windows 7" filter="Speed test" version="10.0" node webdriver.js || result=1
 browserName="internet explorer" platform="Windows 10" filter="Speed test" version="11" node webdriver.js || result=1
 browserName="iphone" platform="Mac 10.11" version="10.3" node webdriver.js || result=1
