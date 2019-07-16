ebcd7b8552711df16a594a48d21dfc2b4a38e064
Bogdan Serhienko <bodis90@gmail.com>
2019-07-16 19:21:21 +0300

    First iteration

--- js/lexer.js
+++ js/lexer.js
@@ -1,16 +1,49 @@
 "use strict";
 
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+var _slicedToArray = function () {
+	function sliceIterator(arr, i) {
+		var _arr = [];
+		var _n = true;
+		var _d = false;
+		var _e = undefined;
+		try {
+			for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) {
+				_arr.push(_s.value);
+				if (i && _arr.length === i) break;
+			}
+		} catch (err) {
+			_d = true;
+			_e = err;
+		} finally {
+			try {
+				if (!_n && _i["return"]) _i["return"]();
+			} finally {
+				if (_d) throw _e;
+			}
+		}
+		return _arr;
+	}
+
+	return function (arr, i) {
+		if (Array.isArray(arr)) {
+			return arr;
+		} else if (Symbol.iterator in Object(arr)) {
+			return sliceIterator(arr, i);
+		} else {
+			throw new TypeError("Invalid attempt to destructure non-iterable instance");
+		}
+	};
+}();
 
 var _require = require("./errors"),
-    getUnclosedTagException = _require.getUnclosedTagException,
-    getUnopenedTagException = _require.getUnopenedTagException,
-    throwMalformedXml = _require.throwMalformedXml;
+	getUnclosedTagException = _require.getUnclosedTagException,
+	getUnopenedTagException = _require.getUnopenedTagException,
+	throwMalformedXml = _require.throwMalformedXml;
 
 var _require2 = require("./doc-utils"),
-    concatArrays = _require2.concatArrays,
-    isTextStart = _require2.isTextStart,
-    isTextEnd = _require2.isTextEnd;
+	concatArrays = _require2.concatArrays,
+	isTextStart = _require2.isTextStart,
+	isTextEnd = _require2.isTextEnd;
 
 var EQUAL = 0;
 var START = -1;
@@ -77,8 +110,8 @@ function tagMatcher(content, textMatchArray, othersMatchArray) {
 		var tagText = content.slice(offset, cursor + 1);
 
 		var _getTag = getTag(tagText),
-		    tag = _getTag.tag,
-		    position = _getTag.position;
+			tag = _getTag.tag,
+			position = _getTag.position;
 
 		var text = allMatches[tag];
 		if (text == null) {
@@ -153,8 +186,8 @@ function splitDelimiters(inside) {
 	}
 
 	var _newDelimiters = _slicedToArray(newDelimiters, 2),
-	    start = _newDelimiters[0],
-	    end = _newDelimiters[1];
+		start = _newDelimiters[0],
+		end = _newDelimiters[1];
 
 	if (start.length === 0 || end.length === 0) {
 		throw new Error("New Delimiters cannot be parsed");
@@ -165,57 +198,24 @@ function splitDelimiters(inside) {
 function getAllIndexes(fullText, delimiters) {
 	var indexes = [];
 	var start = delimiters.start,
-	    end = delimiters.end;
+		end = delimiters.end;
 
-	var offset = -1;
 	while (true) {
-		var startOffset = fullText.indexOf(start, offset + 1);
-		var endOffset = fullText.indexOf(end, offset + 1);
-		var position = null;
-		var len = void 0;
+		var startOffset = -1;
+		var endOffset = -1;
+		const indexOne = fullText.match(/(\[!?[a-zA-Z-_]+[\w]*\])/);
+		if (indexOne) {
+			startOffset = indexOne.index;
+			endOffset = indexOne.index + indexOne[0].length - 1;
+			fullText = fullText.substring(0, startOffset) + '-' + fullText.substring(startOffset + 1);
+
+			indexes.push({ offset: startOffset, position: 'start', length: start.length });
+			indexes.push({ offset: endOffset, position: 'end', length: end.length });
+		}
 		var compareResult = compareOffsets(startOffset, endOffset);
 		if (compareResult === EQUAL) {
 			return indexes;
 		}
-		if (compareResult === END) {
-			offset = endOffset;
-			position = "end";
-			len = end.length;
-		}
-		if (compareResult === START) {
-			offset = startOffset;
-			position = "start";
-			len = start.length;
-		}
-		if (position === "start" && fullText[offset + start.length] === "=") {
-			indexes.push({
-				offset: startOffset,
-				position: "start",
-				length: start.length,
-				changedelimiter: true
-			});
-			var nextEqual = fullText.indexOf("=", offset + start.length + 1);
-			var _endOffset = fullText.indexOf(end, nextEqual + 1);
-
-			indexes.push({
-				offset: _endOffset,
-				position: "end",
-				length: end.length,
-				changedelimiter: true
-			});
-			var insideTag = fullText.substr(offset + start.length + 1, nextEqual - offset - start.length - 1);
-
-			var _splitDelimiters = splitDelimiters(insideTag);
-
-			var _splitDelimiters2 = _slicedToArray(_splitDelimiters, 2);
-
-			start = _splitDelimiters2[0];
-			end = _splitDelimiters2[1];
-
-			offset = _endOffset;
-			continue;
-		}
-		indexes.push({ offset: offset, position: position, length: len });
 	}
 }
 
