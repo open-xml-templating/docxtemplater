1ffbe0c17d38af158cd915301ae9821fd8fec8b9
Edgar HIPP <hipp.edg@gmail.com>
2019-07-24 10:49:24 +0200

    Update pizzip to 3.0.5

--- vendor/download-files.bash
+++ vendor/download-files.bash
@@ -16,6 +16,6 @@ downloadifnotexists() {
 downloadifnotexists https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/1.3.8/FileSaver.min.js
 downloadifnotexists https://cdnjs.cloudflare.com/ajax/libs/mocha/3.5.3/mocha.css
 downloadifnotexists https://cdnjs.cloudflare.com/ajax/libs/mocha/3.5.3/mocha.js
-downloadifnotexists https://unpkg.com/pizzip@3.0.4/dist/pizzip.js
-downloadifnotexists https://unpkg.com/pizzip@3.0.4/dist/pizzip-utils.js
-downloadifnotexists https://unpkg.com/pizzip@3.0.4/dist/pizzip-utils-ie.js
+downloadifnotexists https://unpkg.com/pizzip@3.0.5/dist/pizzip.js
+downloadifnotexists https://unpkg.com/pizzip@3.0.5/dist/pizzip-utils.js
+downloadifnotexists https://unpkg.com/pizzip@3.0.5/dist/pizzip-utils-ie.js
