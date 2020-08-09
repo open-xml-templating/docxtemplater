d09b88caf3e54b9aceb8369219112e9121b0b2b2
Edgar HIPP <hipp.edg@gmail.com>
2020-08-09 11:30:35 +0200

    Add Typescript typings + type tests

---
+++ es6/docxtemplater.d.ts
@@ -0,0 +1,112 @@
+// Type definitions for Docxtemplater 3
+// Project: https://github.com/open-xml-templating/docxtemplater/
+// Definitions by: edi9999 <https://github.com/edi9999>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+// TypeScript Version: 3.9
+
+export namespace DXT {
+    type integer = number;
+
+    interface SimplePart {
+        type: string
+        value: string
+        module?: string
+        [x: string]: any
+    }
+
+    interface Part {
+        type: string
+        value: string
+        module: string
+        raw: string
+        offset: integer
+        lIndex: integer
+        num: integer
+        inverted?: boolean
+        endLIndex?: integer
+        expanded?: Part[]
+        subparsed?: Part[]
+    }
+
+    interface Rendered {
+        value: string
+        errors: any[]
+    }
+
+    type Error = any
+
+    interface Module {
+        set?(options:any): void
+        parse?(placeHolderContent: string): SimplePart | null
+        render?(part: Part): Rendered | null
+        getTraits?(traitName: string, parsed: any): any
+        getFileType?(opts: any): string
+        nullGetter?(part: Part, scopeManager: any) : any
+        optionsTransformer?(options: Options): Options
+        postrender?(parts: string[], options: any) : string[]
+        errorsTransformer?(errors: Error[]) :Error[]
+        getRenderedMap?(map: any): any
+        preparse?(parsed: any, options: any): any
+        postparse?(postparsed: Part[], modules: Module[], options: any): Part[]
+        on?(event :string) :void
+        resolve?(part :Part, options: any): null | Promise<any>
+        [x: string]: any
+    }
+
+    interface ParserContext {
+        meta: {
+            part: Part
+        }
+        scopeList: any[]
+        scopePath: string[]
+        scopePathItem: integer[]
+        scopePathLength: integer[]
+        num: integer
+    }
+
+    interface Parser{
+        get(scope: any, context: ParserContext): any
+    }
+
+    interface ConstructorOptions {
+        modules? : Module[]
+        delimiters? : { start: string, end: string }
+        paragraphLoop? : boolean
+        parser?(tag: string) : Parser
+        linebreaks?: boolean
+        nullGetter?(part: Part) : any
+    }
+
+    interface Options {
+        delimiters? : { start: string, end: string }
+        paragraphLoop? : boolean
+        parser?(tag: string) : Parser
+        linebreaks?: boolean
+        nullGetter?(part: Part) : any
+    }
+}
+
+export class Docxtemplater {
+    /**
+     * Create Docxtemplater instance (and compile it on the fly)
+     *
+     * @param zip Serialized zip archive
+     * @param options modules and other other options
+     */
+    constructor (zip: any, options?: DXT.ConstructorOptions);
+    /**
+     * Create Docxtemplater instance, without options
+     */
+    constructor ();
+
+    setData(data: any): this
+    render() : this
+    getZip() : any
+
+    loadZip(zip: any): this
+    setOptions(options: DXT.Options): this
+    attachModule(module: DXT.Module): this
+    compile(): this
+}
+
+export default Docxtemplater;
---
+++ es6/docxtemplater.test-d.ts
@@ -0,0 +1,69 @@
+import Docxtemplater, {DXT} from "./docxtemplater";
+const PizZip: any = require('pizzip');
+import {expectType, expectError} from 'tsd';
+const doc1 = new Docxtemplater({}, {delimiters: { start: '[[', end: ']]'}, 
+   nullGetter: function(part) {
+        expectError(part.foobar);
+        if (part.module === "rawxml") {
+            return "";
+        }
+        if (part.type === "placeholder" && part.value === "foobar") {
+            return "{Foobar}"
+        }
+        return "Hello";
+    },
+});
+doc1.setData({foo: "bar"});
+doc1.attachModule({
+    set: function() {
+
+    },
+    parse: function(placeHolderContent) {
+        if (placeHolderContent.indexOf(":hello") === 0) {
+            return {
+                type: "placeholder",
+                module: "mycustomModule",
+                value: placeHolderContent.substr(7),
+                isEmpty: "foobar",
+            }
+        }
+        return null;
+    },
+    getFoobar: function() {
+
+    }
+});
+doc1.render()
+
+expectError(doc1.foobar())
+expectError(new Docxtemplater(1,2))
+expectError(new Docxtemplater({}, {delimiters: { start: 1, end: ']]'}}))
+expectError(new Docxtemplater({}, {delimiters: { start: '[['}}))
+
+const doc2 = new Docxtemplater();
+doc2.loadZip(new PizZip("hello"));
+
+// Error because parser should return a {get: fn} object
+expectError(doc2.setOptions({
+    parser: function(tag) {
+        return 10;
+    }
+}));
+
+doc2.setOptions({
+    parser: function(tag) {
+        expectType<string>(tag)
+        return {
+            get: function(scope, context) {
+                const first = context.scopeList[0]
+                expectType<DXT.integer>(context.num)
+                expectError(context.foobar);
+                if (context.meta.part.value === tag) {
+                    return scope[context.meta.part.value];
+                }
+                expectError(context.meta.part.other);
+                return scope[tag];
+            }
+        }
+    }
+});
