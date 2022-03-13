b572a55bfe87fff4117b16bf618c4a5e8525686b
Edgar HIPP <hipp.edg@gmail.com>
2022-03-13 17:49:25 +0100

    Add more assertions to assertion module

--- es6/tests/assertion-module.js
+++ es6/tests/assertion-module.js
@@ -31,9 +31,30 @@ class AssertionModule {
 		}
 		return [];
 	}
-	parse(placeholderContent) {
+	parse(placeholderContent, options) {
 		if (!isString(placeholderContent)) {
 			throw new Error("placeholderContent should be a string");
+		}
+		const { type, position, filePath, contentType, lIndex } = options;
+		if (typeof type !== "string") {
+			throw new Error("parsed contains part without type");
+		}
+		if (type !== "delimiter") {
+			throw new Error(`parsed contains part with invalid type : '${type}'`);
+		}
+		if (position !== "end") {
+			throw new Error(
+				`parsed contains part with invalid position : '${position}'`
+			);
+		}
+		if (typeof filePath !== "string" || filePath.length === 0) {
+			throw new Error("parsed contains part without filePath");
+		}
+		if (typeof contentType !== "string" || contentType.length === 0) {
+			throw new Error("parsed contains part without contentType");
+		}
+		if (!lIndex) {
+			throw new Error("parsed contains part without lIndex");
 		}
 	}
 	postparse(parsed, { filePath, contentType }) {
@@ -46,6 +67,31 @@ class AssertionModule {
 		if (!isString(contentType)) {
 			throw new Error("contentType should be a string");
 		}
+		function logContext(parsed, i) {
+			const context = parsed.slice(i - 2, i + 2);
+			// eslint-disable-next-line no-console
+			console.log(JSON.stringify({ context }));
+		}
+		parsed.forEach(function (part, i) {
+			if (part == null) {
+				logContext(parsed, i);
+				throw new Error("postparsed contains nullish value");
+			}
+			if (!part) {
+				logContext(parsed, i);
+				throw new Error("postparsed contains falsy value");
+			}
+			if (typeof part.type !== "string") {
+				logContext(parsed, i);
+				throw new Error("postparsed contains part without type");
+			}
+			if (["content", "tag", "placeholder"].indexOf(part.type) === -1) {
+				logContext(parsed, i);
+				throw new Error(
+					`postparsed contains part with invalid type : '${part.type}'`
+				);
+			}
+		});
 	}
 	render(part, { filePath, contentType }) {
 		if (!isObject(part)) {
