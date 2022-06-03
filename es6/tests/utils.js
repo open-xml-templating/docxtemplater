b0731d09d3a4dc8dd889ef855470036ccd144f93
Edgar HIPP <hipp.edg@gmail.com>
2022-06-03 15:50:10 +0200

    shouldBeSame : Verify recursively contents of embeddings

--- es6/tests/utils.js
+++ es6/tests/utils.js
@@ -133,6 +133,102 @@ function getText(file) {
 	return file.asText().replace(/\n|\t/g, "");
 }
 
+function isBinaryFile(file1, file2) {
+	return (
+		file1.name.indexOf(".xml") === -1 &&
+		file1.name.indexOf(".rels") === -1 &&
+		(file1.options.binary || file2.options.binary)
+	);
+}
+
+function isZip(text) {
+	const start = text.substr(0, 9);
+	const zipSignature = "PK\u0003\u0004\n\u0000\u0000\u0000\u0000";
+	return start === zipSignature;
+}
+
+function zipCompare(zip, expectedZip, expectedName) {
+	uniq(Object.keys(zip.files).concat(Object.keys(expectedZip.files))).map(
+		function (filePath) {
+			const suffix = `for "${filePath}"`;
+			const file = zip.files[filePath];
+			const expectedFile = expectedZip.files[filePath];
+			expect(expectedFile).to.be.an(
+				"object",
+				`The file ${filePath} doesn't exist on examples/${expectedName}`
+			);
+			expect(file).to.be.an(
+				"object",
+				`The file ${filePath} doesn't exist on ${expectedName}`
+			);
+			expect(file.name).to.be.equal(
+				expectedFile.name,
+				`Name differs ${suffix}`
+			);
+			expect(file.options.dir).to.be.equal(
+				expectedFile.options.dir,
+				`IsDir differs ${suffix}`
+			);
+
+			if (file.options.dir) {
+				return;
+			}
+
+			if (isBinaryFile(file, expectedFile)) {
+				const actualHash = file._data.crc32;
+				const expectedHash = expectedFile._data.crc32;
+
+				const actualText = file.asBinary();
+				if (isZip(actualText)) {
+					const expectedText = expectedFile.asBinary();
+					zipCompare(
+						new PizZip(actualText),
+						new PizZip(expectedText),
+						expectedName
+					);
+				} else if (actualHash && expectedHash) {
+					expect(actualHash).to.be.a("number");
+					expect(actualHash).to.be.equal(
+						expectedHash,
+						"Content differs for " + suffix
+					);
+				} else {
+					const expectedText = expectedFile.asBinary();
+					expect(actualText).to.equal(
+						expectedText,
+						`Binary file ${filePath} differs`
+					);
+				}
+
+				return;
+			}
+			const actualText = getText(file);
+			const expectedText = getText(expectedFile);
+			expect(actualText).to.not.match(
+				emptyNamespace,
+				`The file ${filePath} has empty namespaces`
+			);
+			expect(expectedText).to.not.match(
+				emptyNamespace,
+				`The file ${filePath} has empty namespaces`
+			);
+			if (actualText === expectedText) {
+				return;
+			}
+			const prettyActualText = xmlPrettify(actualText);
+			const prettyExpectedText = xmlPrettify(expectedText);
+
+			if (prettyActualText !== prettyExpectedText) {
+				const prettyDiff = unifiedDiff(prettyActualText, prettyExpectedText);
+				expect(prettyActualText).to.be.equal(
+					prettyExpectedText,
+					"Content differs \n" + suffix + "\n" + prettyDiff
+				);
+			}
+		}
+	);
+}
+
 /* eslint-disable no-console */
 function shouldBeSame({ doc, expectedName }) {
 	const zip = doc.getZip();
@@ -149,81 +245,7 @@ function shouldBeSame({ doc, expectedName }) {
 	const expectedZip = documentCache[expectedName].zip;
 
 	try {
-		uniq(Object.keys(zip.files).concat(Object.keys(expectedZip.files))).map(
-			function (filePath) {
-				const suffix = `for "${filePath}"`;
-				const file = zip.files[filePath];
-				const expectedFile = expectedZip.files[filePath];
-				expect(expectedFile).to.be.an(
-					"object",
-					`The file ${filePath} doesn't exist on examples/${expectedName}`
-				);
-				expect(file).to.be.an(
-					"object",
-					`The file ${filePath} doesn't exist on ${expectedName}`
-				);
-				expect(file.name).to.be.equal(
-					expectedFile.name,
-					`Name differs ${suffix}`
-				);
-				expect(file.options.dir).to.be.equal(
-					expectedFile.options.dir,
-					`IsDir differs ${suffix}`
-				);
-
-				if (file.options.dir) {
-					return;
-				}
-
-				const isBinary =
-					filePath.indexOf(".xml") === -1 &&
-					(file.options.binary || expectedFile.options.binary);
-
-				if (isBinary) {
-					const actualHash = file._data.crc32;
-					if (actualHash) {
-						const expectedHash = expectedFile._data.crc32;
-						expect(actualHash).to.be.a("number");
-						expect(actualHash).to.be.equal(
-							expectedHash,
-							"Content differs for " + suffix
-						);
-					} else {
-						const actualLength = file._data.length;
-						const expectedLength = expectedFile._data.uncompressedSize;
-						expect(actualLength).to.be.a("number");
-						expect(actualLength).to.be.equal(
-							expectedLength,
-							"Content differs for " + suffix
-						);
-					}
-					return;
-				}
-				const actualText = getText(file);
-				const expectedText = getText(expectedFile);
-				expect(actualText).to.not.match(
-					emptyNamespace,
-					`The file ${filePath} has empty namespaces`
-				);
-				expect(expectedText).to.not.match(
-					emptyNamespace,
-					`The file ${filePath} has empty namespaces`
-				);
-				if (actualText === expectedText) {
-					return;
-				}
-				const prettyActualText = xmlPrettify(actualText);
-				const prettyExpectedText = xmlPrettify(expectedText);
-
-				if (prettyActualText !== prettyExpectedText) {
-					const prettyDiff = unifiedDiff(prettyActualText, prettyExpectedText);
-					expect(prettyActualText).to.be.equal(
-						prettyExpectedText,
-						"Content differs \n" + suffix + "\n" + prettyDiff
-					);
-				}
-			}
-		);
+		zipCompare(zip, expectedZip, expectedName);
 	} catch (e) {
 		writeFile(expectedName, zip);
 		console.log(
