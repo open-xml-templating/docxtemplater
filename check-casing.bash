a029dcd9e85506f3fadf5c48813adeae858b1290
Edgar HIPP <hipp.edg@gmail.com>
2023-01-31 09:59:17 +0100

    Update check-casing.bash to avoid /dev/stderr

--- check-casing.bash
+++ check-casing.bash
@@ -9,10 +9,13 @@ tgrep() {
 sort -u |
 ( xargs -d '\n' -- stat -c%n 2>/dev/null  ||: ) > trackedfiles.log
 
-count="$(grep -vE '(Makefile)' <trackedfiles.log | tgrep -v '^docs/' | tgrep -v '\.md$' | tgrep '[A-Z_]' | tee /dev/stderr | wc -l)"
-if [ "$count" = "0" ]
+files="$(grep -vE '(Makefile)' <trackedfiles.log | tgrep -v '^docs/' | tgrep -v '\.md$' | tgrep '[A-Z_]')"
+
+if [ "$files" = "" ]
 then
 	exit 0
 fi
-echo "$count files have incorrect casing"
+
+echo "$(wc -l <<<"$files") files have incorrect casing :"
+printf "%s\n" "$files"
 exit 1
