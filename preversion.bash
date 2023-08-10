af0586b73f202c12aa8da1841453dacc4d51ebc0
Edgar HIPP <hipp.edg@gmail.com>
2023-08-10 08:35:17 -0400

    Reenable browser test

--- preversion.bash
+++ preversion.bash
@@ -10,7 +10,7 @@ mkdirp build
 npm run browserify
 npm run test:typings
 npm run verifypublishsize
-# npm run test:browser
+npm run test:browser
 cp es6/expressions.js es6/expressions-ie11.js js/text.js .
 sed -E -i 's@require...@require(\"./js@g' text.js
 node es6/test-text.js
