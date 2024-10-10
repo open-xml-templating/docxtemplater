e9610b0d3aaee8002ce4aac2ecd3f8b440fb95a8
Edgar HIPP <hipp.edg@gmail.com>
2024-10-10 15:48:46 +0200

    Use only chrome test in preversion

--- preversion.bash
+++ preversion.bash
@@ -11,7 +11,7 @@ mkdirp build
 npm run browserify
 npm run test:typings
 npm run verifypublishsize
-npm run test:browser
+npm run test:chrome
 cp es6/expressions.js es6/expressions-ie11.js js/text.js .
 sed -E -i 's@require\("\.@require(\"./js@g' text.js
 node es6/test-text.js
