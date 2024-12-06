e57df246e9455f8ef490349c35070b627edf68b1
Edgar HIPP <hipp.edg@gmail.com>
2024-12-06 11:24:21 +0100

    Update prettier and eslint ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ import sortClassMembers from "eslint-plugin-sort-class-members";
 export default [
 	{
 		ignores: [
+			"**/.edb-*",
 			"*.sublime-project",
 			"*.sublime-workspace",
 			"npm-debug.log",
