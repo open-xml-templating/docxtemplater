9f7c1b2df8be5b7e951e6a2ca3126a92b7abdedb
Edgar HIPP <hipp.edg@gmail.com>
2026-02-24 12:04:20 +0100

    Update errors documentation

--- es6/errors.js
+++ es6/errors.js
@@ -46,6 +46,14 @@ XTAPIVersionError.prototype = new XTError();
 
 function throwApiVersionError(msg, properties) {
 	const err = new XTAPIVersionError(msg);
+	/*
+	 * This error arises when a recent module version necessitates a more
+	 * current version of the docxtemplater core. Docxtemplater specifies an
+	 * "APIVersion," and if a module requires API Version 3.55 or higher,
+	 * but the docxtemplater instance provides API 3.52, this error will
+	 * occur. To resolve this issue, please update to the latest version of
+	 * docxtemplater.
+	 */
 	err.properties = {
 		id: "api_version_error",
 		...properties,
@@ -55,6 +63,19 @@ function throwApiVersionError(msg, properties) {
 
 function throwMultiError(errors) {
 	const err = new XTTemplateError("Multi error");
+	/*
+	 * This error is an Error that contains all template errors.
+	 * It is a multi error because it contains all errors of the template in :
+	 * err.properties.errors.
+	 *
+	 * You can then map on each sub error like this :
+	 *
+	 * ```js
+	 * for (const err of error.properties.errors) {
+	 *   console.log(err.properties.explanation);
+	 * }
+	 * ```
+	 */
 	err.properties = {
 		errors,
 		id: "multi_error",
@@ -65,6 +86,14 @@ function throwMultiError(errors) {
 
 function getUnopenedTagException(options) {
 	const err = new XTTemplateError("Unopened tag");
+	/*
+	 * This error happens if a tag is closed but not opened. For example with
+	 * the following template:
+	 *
+	 * ```docx
+	 * Hello name} !
+	 * ```
+	 */
 	err.properties = {
 		xtag: last(options.xtag.split(" ")),
 		id: "unopened_tag",
@@ -83,6 +112,13 @@ function getDuplicateOpenTagException(options) {
 	const err = new XTTemplateError(
 		"Duplicate open tag, expected one open tag"
 	);
+	/*
+	 * This error happens with following template :
+	 *
+	 * ```docx
+	 * Hello {{name
+	 * ```
+	 */
 	err.properties = {
 		xtag: first(options.xtag.split(" ")),
 		id: "duplicate_open_tag",
@@ -101,6 +137,13 @@ function getDuplicateCloseTagException(options) {
 	const err = new XTTemplateError(
 		"Duplicate close tag, expected one close tag"
 	);
+	/*
+	 * This error happens with following template :
+	 *
+	 * ```docx
+	 * Hello {name}}
+	 * ```
+	 */
 	err.properties = {
 		xtag: first(options.xtag.split(" ")),
 		id: "duplicate_close_tag",
@@ -117,8 +160,17 @@ function getDuplicateCloseTagException(options) {
 
 function getUnclosedTagException(options) {
 	const err = new XTTemplateError("Unclosed tag");
+	/*
+	 * This error happens if a tag is opened but not closed.
+	 * For example with the following template:
+	 *
+	 * ```docx
+	 * Hello {name !
+	 * ```
+	 *
+	 */
 	err.properties = {
-		xtag: first(options.xtag.split(" ")).substr(1),
+		xtag: first(options.xtag.split(" ")).substr(1), // name
 		id: "unclosed_tag",
 		context: options.xtag,
 		offset: options.offset,
@@ -132,24 +184,70 @@ function getUnclosedTagException(options) {
 }
 
 function throwXmlTagNotFound(options) {
+	if (options.position === "left") {
+		throwXmlTagNotFoundLeft(options);
+	} else {
+		throwXmlTagNotFoundRight(options);
+	}
+}
+
+function throwXmlTagNotFoundLeft(options) {
 	const err = new XTTemplateError(
 		`No tag "${options.element}" was found at the ${options.position}`
 	);
 	const part = options.parsed[options.index];
+	/*
+	 * This error is not directly linked to the template, it means that some
+	 * tag tried to expand to adjacent XML tags, but those elements cannot be
+	 * accessed from the current node.
+	 *
+	 * This error happens if a rawXMLTag doesn't find a `<w:p>` element
+	 *
+	 * ```docx
+	 * <w:p><w:t>{@raw}</w:t>
+	 * // Note  that the `</w:p>` tag is missing.
+	 * ```
+	 */
 	err.properties = {
-		id: `no_xml_tag_found_at_${options.position}`,
-		explanation: `No tag "${options.element}" was found at the ${options.position}`,
+		id: "no_xml_tag_found_at_left",
+		explanation: `No tag "${options.element}" was found at the left`,
 		offset: part.offset,
 		part,
 		parsed: options.parsed,
 		index: options.index,
 		element: options.element,
 	};
-	throw err;
+}
+
+function throwXmlTagNotFoundRight(options) {
+	const err = new XTTemplateError(
+		`No tag "${options.element}" was found at the ${options.position}`
+	);
+	const part = options.parsed[options.index];
+	/*
+	 * This error is not directly linked to the template, it means that some
+	 * tag tried to expand to adjacent XML tags, but those elements cannot be
+	 * accessed from the current node.
+	 */
+	err.properties = {
+		id: "no_xml_tag_found_at_left",
+		explanation: `No tag "${options.element}" was found at the left`,
+		offset: part.offset,
+		part,
+		parsed: options.parsed,
+		index: options.index,
+		element: options.element,
+	};
 }
 
 function getCorruptCharactersException({ tag, value, offset }) {
 	const err = new XTRenderingError("There are some XML corrupt characters");
+	/*
+	 * This error prevents the docx document to become corrupt.
+	 * It happens if you're trying to render text that would produce invalid XML output.
+	 *
+	 * See #corrupt-character-error on how this can be fixed by changing your parser.
+	 */
 	err.properties = {
 		id: "invalid_xml_characters",
 		xtag: tag,
@@ -164,6 +262,13 @@ function getInvalidRawXMLValueException({ tag, value, offset, partDelims }) {
 	const err = new XTRenderingError(
 		"Non string values are not allowed for rawXML tags"
 	);
+	/*
+	 * This error happens if you try to render a rawXml tag, such as : {@raw}
+	 * And the value of the data for "raw" is truthy but not a string.
+	 *
+	 * (If the value of the data is falsy, than the tag is simply dropped and
+	 * no error is thrown)
+	 */
 	err.properties = {
 		id: "invalid_raw_xml_value",
 		xtag: tag,
@@ -186,6 +291,10 @@ function throwExpandNotFound(options) {
 	if (typeof explanation === "function") {
 		explanation = explanation(part);
 	}
+	/*
+	 * This error happens if you try to render a rawXml tag, such as : {@raw},
+	 * but that tag is not placed inside a paragraph.
+	 */
 	const err = new XTTemplateError(message);
 	err.properties = {
 		id,
@@ -205,6 +314,36 @@ function throwRawTagShouldBeOnlyTextInParagraph(options) {
 		"Raw tag should be the only text in paragraph"
 	);
 	const tag = options.part.value;
+	/*
+	 * This happens when a rawXMLTag {@raw} is not the only text in the
+	 * paragraph. For example, writing ` {@raw}` (Note the spaces)
+	 * is not acceptable because the {@raw} tag replaces the full paragraph. We
+	 * prefer to throw an Error now rather than have "strange behavior"
+	 * because the spaces "disappeared".
+	 *
+	 * To correct this error, you have to add manually the text that you want
+	 * in your raw tag. (Or you can use the [docxtemplater word-run
+	 * module](/modules/word-run/) which adds a tag
+	 * that can replace rawXML inside a tag).
+	 *
+	 * Writing
+	 *
+	 * ```docx
+	 * {@my_first_tag}{my_second_tag}
+	 * ```
+	 *
+	 * Or even
+	 *
+	 * ```docx
+	 * Hello {@my_first_tag}
+	 * ```
+	 *
+	 * Is misusing docxtemplater.
+	 *
+	 * The `@` at the beginning means "replace the xml of **the
+	 * current paragraph** with scope.my_first_tag" so that means that
+	 * everything else in that Paragraph will be removed.
+	 */
 	err.properties = {
 		id: "raw_xml_tag_should_be_only_text_in_paragraph",
 		explanation: `The raw tag "${tag}" should be the only text in this paragraph. This means that this tag should not be surrounded by any text or spaces.`,
@@ -219,9 +358,16 @@ function getUnmatchedLoopException(part) {
 	const { location, offset, square } = part;
 	const t = location === "start" ? "unclosed" : "unopened";
 	const T = location === "start" ? "Unclosed" : "Unopened";
-
 	const err = new XTTemplateError(`${T} loop`);
 	const tag = part.value;
+	/*
+	 * This error happens with following template :
+	 *
+	 * ```docx
+	 * {#users}
+	 * {/companies}
+	 * ```
+	 */
 	err.properties = {
 		id: `${t}_loop`,
 		explanation: `The loop with tag "${tag}" is ${t}`,
@@ -240,6 +386,26 @@ function getUnbalancedLoopException(pair, lastPair) {
 	const lastR = lastPair[1].part.value;
 	const l = pair[0].part.value;
 	const r = pair[1].part.value;
+	/*
+	 * This error happens if you create a table and misplace tags inside the table :
+	 *
+	 * ```docx
+	 * -------------------------------
+	 * | {#a}X       | {/a}{#b}Y{/b} |
+	 * -------------------------------
+	 * ```
+	 *
+	 *  In the case above, the {#a} and {/a} will expand to the whole loop, but this is not possible because of the other loop in {#b}Y{/b}
+	 *
+	 *  Instead, you should usually write :
+	 *
+	 * ```docx
+	 * -------------------------------
+	 * | {#a}X       | {#b}Y{/b}{/a} |
+	 * -------------------------------
+	 * ```
+	 *
+	 */
 	err.properties = {
 		id: "unbalanced_loop_tags",
 		explanation: `Unbalanced loop tags {#${lastL}}{/${lastR}}{#${l}}{/${r}}`,
@@ -258,6 +424,17 @@ function getUnbalancedLoopException(pair, lastPair) {
 
 function getClosingTagNotMatchOpeningTag({ tags }) {
 	const err = new XTTemplateError("Closing tag does not match opening tag");
+	/*
+	 * This error happens if your loop tags are incorrectly closed
+	 *
+	 * ```docx
+	 * {#condition1}
+	 * Some text
+	 * {/otherCondition}
+	 * ```
+	 *
+	 * Since the start tag does not match the open tag, the template is invalid.
+	 */
 	err.properties = {
 		id: "closing_tag_does_not_match_opening_tag",
 		explanation: `The tag "${tags[0].value}" is closed by the tag "${tags[1].value}"`,
@@ -273,6 +450,21 @@ function getClosingTagNotMatchOpeningTag({ tags }) {
 
 function getScopeCompilationError({ tag, rootError, offset }) {
 	const err = new XTScopeParserError("Scope parser compilation failed");
+	/*
+	 * This happens when your parser throws an error during compilation. The
+	 * parser is the second argument of the constructor
+	 * `new Docxtemplater(zip, {parser: function parser(tag) {}});`
+	 *
+	 * For example, if your template is:
+	 *
+	 * ```docx
+	 * {name++}
+	 * ```
+	 *
+	 * and you use the angular expression parser, you will have this error. The error
+	 * happens when you call parser('name++'); The underlying error can be
+	 * read in `e.properties.rootError`
+	 */
 	err.properties = {
 		id: "scopeparser_compilation_failed",
 		offset,
@@ -285,6 +477,42 @@ function getScopeCompilationError({ tag, rootError, offset }) {
 
 function getScopeParserExecutionError({ tag, scope, error, offset }) {
 	const err = new XTScopeParserError("Scope parser execution failed");
+	/*
+	 * This happens when your parser throws an error during execution. The
+	 * parser is the second argument of the constructor
+	 * `new Docxtemplater(zip, {parser: function parser(tag) {}});`
+	 *
+	 * For example, if your template is:
+	 *
+	 * ```docx
+	 * {test | toFixed}
+	 * ```
+	 *
+	 * and your code is :
+	 *
+	 * ```js
+	 * const expressionParser = require("docxtemplater/expressions.js");
+	 * const doc = new Docxtemplater(zip, {
+	 *   paragraphLoop: true,
+	 *   linebreaks: true,
+	 *   parser: expressionParser.configure({
+	 *     filters: {
+	 *       toFixed(input) {
+	 *         return input.toFixed();
+	 *       }
+	 *     }
+	 *   }),
+	 * });
+	 * doc.render({
+	 *   test: false
+	 * });
+	 * ```
+	 *
+	 * Since false.toFixed() triggers an error in Javascript, this will then throw an error "Scope parser execution failed".
+	 *
+	 * You can either fix your data or make your toFixed function more robust
+	 * by returning "input" if the input is not a number.
+	 */
 	err.properties = {
 		id: "scopeparser_execution_failed",
 		explanation: `The scope parser for the tag ${tag} failed to execute`,
@@ -300,6 +528,26 @@ function getLoopPositionProducesInvalidXMLError({ tag, offset }) {
 	const err = new XTTemplateError(
 		`The position of the loop tags "${tag}" would produce invalid XML`
 	);
+	/*
+	 * ### loop_position_invalid
+	 *
+	 * This happens when a loop would produce invalid XML.
+	 *
+	 * For example, if you write:
+	 *
+	 * ```docx
+	 * ======================
+	 * | header1  | header2 |
+	 * ----------------------
+	 * | {#users} | content |
+	 * ======================
+	 *
+	 * {/users}
+	 * ```
+	 *
+	 * this is not allowed since a loop that starts in a table must also end
+	 * in that table.
+	 */
 	err.properties = {
 		xtag: tag,
 		id: "loop_position_invalid",
@@ -315,6 +563,11 @@ function throwUnimplementedTagType(part, index) {
 		errorMsg += ` "${part.module}"`;
 	}
 	const err = new XTTemplateError(errorMsg);
+	/*
+	 * This happens when a tag type is not implemented. It should normally not happen,
+	 * unless you changed docxtemplater code or created your own module and didn't
+	 * implement the `render` function of your module correctly.
+	 */
 	err.properties = {
 		part,
 		index,
@@ -325,6 +578,10 @@ function throwUnimplementedTagType(part, index) {
 
 function throwMalformedXml() {
 	const err = new XTInternalError("Malformed xml");
+	/*
+	 * This happens when an xml file of the document cannot be parsed
+	 * correctly.
+	 */
 	err.properties = {
 		explanation: "The template contains malformed xml",
 		id: "malformed_xml",
@@ -336,6 +593,13 @@ function throwResolveBeforeCompile() {
 	const err = new XTInternalError(
 		"You must run `.compile()` before running `.resolveData()`"
 	);
+	/*
+	 * This happens if you're calling `resolveData()` before you run `.compile()`.
+	 *
+	 * You should always call `compile` first and then only `resolveData`
+	 *
+	 * Or you can migrate to [the constructor with two arguments](/docs/get-started-node/#usage)
+	 */
 	err.properties = {
 		id: "resolve_before_compile",
 		explanation:
@@ -348,6 +612,9 @@ function throwRenderInvalidTemplate() {
 	const err = new XTInternalError(
 		"You should not call .render on a document that had compilation errors"
 	);
+	/*
+	 * This happens if you're calling `render()` on a document that had template errors
+	 */
 	err.properties = {
 		id: "render_on_invalid_template",
 		explanation:
@@ -360,6 +627,11 @@ function throwRenderTwice() {
 	const err = new XTInternalError(
 		"You should not call .render twice on the same docxtemplater instance"
 	);
+	/*
+	 * This happens if you're calling `render()` on a document twice.
+	 *
+	 * You should always create a new docxtemplater instance if you need to create two output documents.
+	 */
 	err.properties = {
 		id: "render_twice",
 		explanation:
@@ -380,6 +652,13 @@ function throwFileTypeNotIdentified(zip) {
 	const err = new XTInternalError(
 		`The filetype for this file could not be identified, is this file corrupted ? ${msg}`
 	);
+	/*
+	 * This error happens if you're creating docxtemplater with a zip file, but that file is not recognized as a docx/pptx/xlsx or odt file.
+	 *
+	 * Note that xlsx files and odt files need a paid module to be templated.
+	 *
+	 * Other zip files (zip, odp, ods) will trigger the same error.
+	 */
 	err.properties = {
 		id: "filetype_not_identified",
 		explanation: `The filetype for this file could not be identified, is this file corrupted ? ${msg}`,
@@ -389,6 +668,13 @@ function throwFileTypeNotIdentified(zip) {
 
 function throwXmlInvalid(content, offset) {
 	const err = new XTTemplateError("An XML file has invalid xml");
+	/*
+	 * This error happens if the XML is invalid in your template file.
+	 *
+	 * This should be very rare except if you were using a tool to preprocess
+	 * the template (an XML error in a docx file means that the template file
+	 * is already corrupt).
+	 */
 	err.properties = {
 		id: "file_has_invalid_xml",
 		content,
@@ -402,6 +688,10 @@ function throwFileTypeNotHandled(fileType) {
 	const err = new XTInternalError(
 		`The filetype "${fileType}" is not handled by docxtemplater`
 	);
+	/*
+	 * This error happens if the filetype was recognized (xlsx, odt), but
+	 * without the correct module, this file cannot be templated
+	 */
 	err.properties = {
 		id: "filetype_not_handled",
 		explanation: `The file you are trying to generate is of type "${fileType}", but only docx and pptx formats are handled`,
